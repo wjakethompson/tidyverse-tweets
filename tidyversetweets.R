a38dfb85a76c7355b48f9538b9d366ffc87df359
Jake Thompson <wjakethompson@gmail.com>
2017-11-28 13:12:18 -0600

    add script for query and tweet

---
+++ tidyversetweets.R
@@ -0,0 +1,27 @@
+### Setup ----------------------------------------------------------------------
+needed_packages <- c("devtools", "tidyverse", "glue", "stackr", "rtweet")
+load_packages <- function(x) {
+  if (!(x %in% rownames(installed.packages()))) {
+    if (x == "stackr") {
+      devtools::install_github("dgrtwo/stackr")
+    } else {
+      install.packages(x, repos = "https://cran.rstudio.com/")
+    }
+  }
+  suppressPackageStartupMessages(require(x, character.only = TRUE))
+}
+vapply(needed_packages, load_packages, TRUE)
+
+
+### Query Stackoverflow API ----------------------------------------------------
+tidy_so <- stack_questions(pagesize = 100, tagged = "tidyverse") %>%
+  select(title, creation_date, link) %>%
+  mutate(creation_date = ymd_hms(creation_date)) %>%
+  arrange(desc(creation_date)) %>%
+  filter(creation_date > ymd_hms(Sys.time()) - dminutes(5)) %>%
+  as.list()
+
+pwalk(.l = tidy_so, .f = function(title, creation_date, link) {
+  tweet_text <- glue("{title} #tidyverse #rstats {link}")
+  post_tweet(tweet_text)
+})
