0808beb6af1f4d2742e4689d96ecf61c631063d2
Jake Thompson <wjakethompson@gmail.com>
2017-11-28 15:00:00 -0600

    job submission script

---
+++ tweet-job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#MSUB -N tidytweet
+#MSUB -l nodes=1:ppn=1:ib,walltime=00:00:10:00
+#MSUB -l pmem=2gb
+#MSUB -M tidytweet@gmail.com
+#MSUB -m a
+#MSUB -j oe
+#MSUB -o tidytweet.log
+#MSUB -q sixhour
+
+cd $PBS_O_WORKDIR
+
+R --vanilla -f tidyversetweets.R
