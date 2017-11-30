24d89a695b0f97a8e3cede768afe79eb9afccc45
Jake Thompson <wjakethompson@gmail.com>
2017-11-30 11:23:54 -0600

    fix catastrophe where it attempted to tweet out all SO tags available

--- tidyversetweets.R
+++ tidyversetweets.R
@@ -26,7 +26,10 @@ source("~/.Rprofile")
 tidy_so <- map(tidyverse, query_tag) %>%
   map_dfr(~(.$result %>% as.tibble())) %>%
   select(title, creation_date, link) %>%
-  mutate(title = str_replace_all(title, "&#39;", "'")) %>%
+  mutate(
+    title = str_replace_all(title, "&#39;", "'"),
+    title = str_replace_all(title, "&quot;", '"')
+  ) %>%
   distinct() %>%
   mutate(creation_date = with_tz(creation_date, tz = "America/Chicago")) %>%
   arrange(creation_date)
@@ -40,9 +43,10 @@ tidy_rc <- feed.extract("https://community.rstudio.com/posts.rss") %>%
 
 all_update <- bind_rows(tidy_so, tidy_rc) %>%
   arrange(creation_date) %>%
-  filter(creation_date > cur_time - dminutes(5))
+  filter(creation_date > cur_time - dminutes(5)) %>%
+  as.list()
 
-pwalk(.l = tidy_so, .f = function(title, creation_date, link) {
+pwalk(.l = all_update, .f = function(title, creation_date, link) {
   if (nchar(title) > 250) {
     trunc_points <- str_locate_all(title, " ") %>%
       .[[1]] %>%
