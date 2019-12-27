8671cbd693609ccc16eb7e72eb5704f25cc56d11
Jake Thompson <wjakethompson@gmail.com>
2019-12-27 13:37:11 -0600

    update tweet job

--- tweet-job.sh
+++ tweet-job.sh
@@ -1,13 +1,17 @@
-#!/bin/bash
-#MSUB -N tidytweet
-#MSUB -l nodes=1:ppn=1:ib,walltime=00:00:10:00
-#MSUB -l pmem=5gb
-#MSUB -M tidytweet@gmail.com
-#MSUB -m a
-#MSUB -j oe
-#MSUB -o tidytweet.log
-#MSUB -q sixhour
+#!/bin/bash -l
+#SBATCH --job-name=tidytweet
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --constraint "ib"
+#SBATCH --mem-per-cpu=5gb
+#SBATCH --time=0-00:10:00
+#SBATCH --mail-user=tidytweet@gmail.com
+#SBATCH --mail-type=FAIL
+#SBATCH --output=tidytweet.log
+#SBATCH --partition=sixhour
 
-cd $PBS_O_WORKDIR
+cd $SLURM_SUBMIT_DIR
+
+module load R rstan
 
 R --vanilla -f tidyversetweets.R
