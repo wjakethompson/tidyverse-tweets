fed939286e0c217a5ad44e922bda66348bec1c99
Jake Thompson <wjakethompson@gmail.com>
2019-12-27 13:24:58 -0600

    use last tweet as reference

--- tidyversetweets.R
+++ tidyversetweets.R
@@ -25,9 +25,6 @@ tidyverse <- c("tidyverse", "ggplot2", "dplyr", "tidyr", "readr", "purrr",
   "stringr", "lubridate", "forcats", "hms", "blob;r", "rlang", "magrittr",
   "glue", "recipes", "rsample", "modelr")
 
-Sys.setenv(TZ = "America/Chicago")
-cur_time <- ymd_hms(Sys.time(), tz = Sys.timezone())
-
 source("~/.Rprofile")
 
 tidy_so <- map(tidyverse, query_tag) %>%
@@ -42,7 +39,7 @@ tidy_so <- map(tidyverse, query_tag) %>%
     title = str_replace_all(title, "&amp;", "&")
   ) %>%
   distinct() %>%
-  mutate(creation_date = with_tz(creation_date, tz = "America/Chicago")) %>%
+  mutate(creation_date = with_tz(creation_date, tz = "UTC")) %>%
   arrange(creation_date)
 
 rstudio <- c("tidyverse", "teaching", "general", "R-Markdown", "shiny",
@@ -51,17 +48,22 @@ rstudio <- c("tidyverse", "teaching", "general", "R-Markdown", "shiny",
 tidy_rc <- map(rstudio, query_community) %>%
   map_dfr(~(.$items %>% as_tibble())) %>%
   select(title, creation_date = date, link) %>%
-  mutate(creation_date = with_tz(creation_date, tz = "America/Chicago")) %>%
+  mutate(creation_date = with_tz(creation_date, tz = "UTC")) %>%
   group_by(title) %>%
   top_n(n = -1, wt = creation_date) %>%
   arrange(creation_date)
 
+last_tweet <- get_timeline("tidyversetweets", n = 100,
+                           token = read_rds("~/.rtweet_token2.rds")) %>%
+  top_n(1, wt = created_at) %>%
+  pull(created_at) %>%
+  with_tz(tz = "UTC")
+
 all_update <- bind_rows(tidy_so, tidy_rc) %>%
   arrange(creation_date) %>%
-  filter(creation_date > cur_time - dminutes(5)) %>%
-  as.list()
+  filter(creation_date > last_tweet)
 
-pwalk(.l = all_update, .f = function(title, creation_date, link) {
+pwalk(all_update, .f = function(title, creation_date, link) {
   if (nchar(title) > 250) {
     trunc_points <- str_locate_all(title, " ") %>%
       .[[1]] %>%
