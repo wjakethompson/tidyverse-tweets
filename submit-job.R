ac718f683c3eaec6604991bb75cefc39349db211
Jake Thompson <wjakethompson@gmail.com>
2020-02-28 14:47:45 -0600

    update job submission

--- submit-job.R
+++ submit-job.R
@@ -1,12 +1,19 @@
 # cron job
-# */5 * * * * cd tidyverse-tweets && chmod +x submit-job.sh && submit-job.sh
+# */5 * * * * cd tidyverse-tweets && chmod +x submit-job.sh && bash submit-job.sh
 
 library(readr)
+library(purrr)
 library(glue)
 
-active_jobs <- read_delim(system2("squeue", "-u w449t405", stdout = TRUE),
-                          delim = " ", trim_ws = TRUE)
+safe_read_delim <- safely(read_delim)
 
-if (!("tidytwee" %in% active_jobs$NAME)) {
+active_jobs <- safe_read_delim(system2("squeue", "-u w449t405", stdout = TRUE),
+                               delim = " ", trim_ws = TRUE)
+
+if (is.null(active_jobs$result)) {
   system2("sbatch", "tweet-job.sh")
+} else {
+  if (!("tidytwee" %in% active_jobs$NAME)) {
+    system2("sbatch", "tweet-job.sh")
+  }
 }
