53eeb9b1fba733d56308584c26043fe79207461b
Jake Thompson <wjakethompson@gmail.com>
2021-04-07 12:16:34 -0500

    update test script

--- test-tweet.R
+++ test-tweet.R
@@ -1,4 +1,18 @@
-library(tidyverse)
 library(rtweet)
+library(lubridate)
 
-post_tweet("test tweet", token = read_rds("~/.rtweet_token2.rds"))
+cur_time <- now()
+
+with_tz(cur_time, tzone = "UTC")
+with_tz(cur_time, tzone = "America/Chicago")
+
+create_token(
+  app = "tidyverse_tweets",
+  consumer_key = Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+  consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+  access_token = Sys.getenv("TWITTER_ACCESS_TOKEN"),
+  access_secret = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
+  set_renv = FALSE
+)
+
+post_tweet("test tweet")
