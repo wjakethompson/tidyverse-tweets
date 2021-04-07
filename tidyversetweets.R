8fe493c352f9bcf7391e4bfeed45c2deeacb40af
Jake Thompson <wjakethompson@gmail.com>
2021-04-07 12:16:23 -0500

    update bot script

--- tidyversetweets.R
+++ tidyversetweets.R
@@ -1,12 +1,14 @@
 ### Setup ----------------------------------------------------------------------
-library(tidyverse)
 library(glue)
 library(lubridate)
 library(stackr)
 library(feedeR)
 library(rtweet)
+library(purrr)
+library(tibble)
+library(dplyr)
+library(stringr)
 
-Sys.info()
 
 ### Functions ------------------------------------------------------------------
 fix_special <- function(x) {
@@ -29,18 +31,14 @@ query_tag <- function(tag) {
   query <- safe_query(pagesize = 100, tagged = tag)
   return(query)
 }
-query_community <- function(category) {
-  query <- feed.extract(glue("https://community.rstudio.com/c/{category}.rss"))
-  return(query)
-}
+
+Sys.setenv(STACK_EXCHANGE_KEY = "OX0Slpm1*EXldYd5wMEH8g((")
 
 tidyverse <- c("tidyverse", "ggplot2", "dplyr", "tidyr", "readr", "purrr",
-  "tibble", "readxl", "haven", "jsonlite", "xml2", "httr", "rvest", "DBI;r",
-  "stringr", "lubridate", "forcats", "hms;r", "blob;r", "rlang", "magrittr",
-  "glue", "recipes", "rsample", "modelr", "r-markdown", "bookdown", "blogdown",
-  "xaringan")
-
-source("~/.Rprofile")
+               "tibble", "readxl", "haven", "jsonlite", "xml2", "httr", "rvest",
+               "DBI;r", "stringr", "lubridate", "forcats", "hms;r", "blob;r",
+               "rlang", "magrittr", "glue", "recipes", "rsample", "modelr",
+               "r-markdown", "bookdown", "blogdown", "xaringan")
 
 tidy_so <- map(tidyverse, query_tag) %>%
   map_dfr(~(.$result %>% as_tibble())) %>%
@@ -59,8 +57,15 @@ tidy_so <- map(tidyverse, query_tag) %>%
     title = str_replace_all(title, "&#233;", "\u00e9")
   ) %>%
   distinct() %>%
-  mutate(creation_date = with_tz(creation_date, tz = "UTC")) %>%
+  mutate(creation_date = with_tz(creation_date, tzone = "UTC")) %>%
   arrange(creation_date)
+
+
+### Query RStudio Community ----------------------------------------------------
+query_community <- function(category) {
+  query <- feed.extract(glue("https://community.rstudio.com/c/{category}.rss"))
+  return(query)
+}
 
 rstudio <- c("tidyverse", "teaching", "general", "R-Markdown", "shiny",
              "package-development", "ml")
@@ -72,16 +77,26 @@ tidy_rc <- map(rstudio, query_community) %>%
     title = str_replace_all(title, "\u2018|\u2019", "'"),
     title = map_chr(title, fix_special)
   ) %>%
-  mutate(creation_date = with_tz(creation_date, tz = "UTC")) %>%
+  mutate(creation_date = with_tz(creation_date, tzone = "UTC")) %>%
   group_by(title) %>%
-  top_n(n = -1, wt = creation_date) %>%
+  slice_min(order_by = creation_date, n = 1) %>%
   arrange(creation_date)
 
-last_tweet <- get_timeline("tidyversetweets", n = 100,
-                           token = read_rds("~/.rtweet_token2.rds")) %>%
-  top_n(1, wt = created_at) %>%
+
+### Tweet ----------------------------------------------------------------------
+create_token(
+  app = "tidyverse_tweets",
+  consumer_key = Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+  consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+  access_token = Sys.getenv("TWITTER_ACCESS_TOKEN"),
+  access_secret = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
+  set_renv = FALSE
+)
+
+last_tweet <- get_timeline("tidyversetweets", n = 100) %>%
+  slice_max(order_by = created_at, n = 1) %>%
   pull(created_at) %>%
-  with_tz(tz = "UTC")
+  with_tz(tzone = "UTC")
 
 all_update <- bind_rows(tidy_so, tidy_rc) %>%
   arrange(creation_date) %>%
@@ -97,5 +112,5 @@ pwalk(all_update, .f = function(title, creation_date, link) {
   }
   
   tweet_text <- glue("{title} #tidyverse #rstats {link}")
-  post_tweet(tweet_text, token = read_rds("~/.rtweet_token2.rds"))
+  post_tweet(tweet_text)
 })
